9f905d8edebe998ecbe55be2af19905382b228f7
Tobias Duarte <tobiiduarte95@gmail.com>
2023-12-13 20:16:39 -0300

    middlewares

--- models/server.ts
+++ models/server.ts
@@ -20,7 +20,7 @@ export class Server {
     this.issuesPath = "/issues";
 
     this.conectarDB();
-    this.middleware();
+    this.middlewares();
     this.routes();
   }
 
@@ -28,14 +28,9 @@ export class Server {
     await dbConnection();
   }
 
-  middleware(): void {
+  middlewares(): void {
     this.app.use(cors());
     this.app.use(express.json());
-    this.app.use((req, res, next) => {
-      res.header("Access-Control-Allow-Origin", "*");
-      res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-      next();
-    });
   }
 
   routes(): void {
