431e54db1ce5b9d800211d28656f93a2d9358caf
Tobias Duarte <tobiiduarte95@gmail.com>
2023-12-17 20:16:58 -0300

    vercel problems

--- dataBase/config.ts
+++ dataBase/config.ts
@@ -4,10 +4,13 @@ export const dbConnection = async (): Promise<void> => {
   try {
     const dbURL = process.env.DB_URL;
     if (!dbURL) {
+      console.log(Error);
       throw new Error("URL no encontrada en la base de datos");
     }
 
     await mongoose.connect(dbURL);
+
+    console.log("Base de datos online");
   } catch (error) {
     console.log(error);
     throw new Error("Error al iniciar la base de datos");
